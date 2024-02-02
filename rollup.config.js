7fe969625a3f01ca67e65e3b4fe9c08acd60f8cc
Isaac <isaaconyes80@gmail.com>
2024-02-02 17:28:21 +0100

    @rollup/plugin-replace

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import typescript from '@rollup/plugin-typescript'
-import { dts } from 'rollup-plugin-dts'
-import postcss from 'rollup-plugin-postcss'
-import json from '@rollup/plugin-json'
- 
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { dts } from 'rollup-plugin-dts';
+import postcss from 'rollup-plugin-postcss';
+import json from '@rollup/plugin-json';
+import replace from '@rollup/plugin-replace'; // Import the plugin
 
 export default [
     {
@@ -25,20 +25,23 @@ export default [
             resolve(),
             commonjs(),
             typescript({ tsconfig: './tsconfig.json' }),
-            postcss(
-                {
-                    extensions: ['.css'],
-                    minimize: true,
-                    sourceMap: true,
-                    modules: true,
-                }
-            ),
+            postcss({
+                extensions: ['.css'],
+                minimize: true,
+                sourceMap: true,
+                modules: true,
+            }),
             json(),
-        ]
+            replace({
+                'process.env.NODE_DEBUG': JSON.stringify(''), // Mock NODE_DEBUG or any other variables
+                preventAssignment: true,
+            }),
+            // Add more replacements as needed
+        ],
     },
     {
         input: 'dist/esm/types/index.d.ts',
         output: [{ file: 'dist/index.d.ts', format: 'esm' }],
         plugins: [dts()],
-    }
-]
+    },
+];
